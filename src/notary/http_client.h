350ecdfbc2c2a211d6977db10f3e4d48f4caa734
zhujianguo <zjg555543@163.com>
2019-08-19 14:52:47 +0800

    Add http_client.h

---
+++ src/notary/http_client.h
@@ -0,0 +1,11 @@
+#ifndef HTTP_CLIENT_H_
+#define HTTP_CLIENT_H_
+
+#include <curl/curl.h>
+#include <utils/headers.h>
+
+std::string HttpGet(std::string url);
+
+std::string HttpPost(const std::string &url, const std::string &post_data);
+
+#endif
