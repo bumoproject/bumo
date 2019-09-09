f67e02486d3046fc8aa72ff3f2c8eb43073e6414
zhujianguo <zjg555543@163.com>
2019-09-09 14:17:26 +0800

    Modify http_client.h

--- src/notary/http_client.h
+++ src/notary/http_client.h
@@ -4,8 +4,10 @@
 #include <curl/curl.h>
 #include <utils/headers.h>
 
+//http client for get
 std::string HttpGet(std::string url);
 
+//http client for post
 std::string HttpPost(const std::string &url_address, const std::string &post_data);
 
 #endif
